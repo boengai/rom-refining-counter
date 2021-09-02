84003f43d6955834a032c2fad75215d3d09e670a
Boe Khattiya <keerati.khat@gmail.com>
2021-09-03 00:07:42 +0700

    🍱: rename

--- src/components/ImgPoring.tsx
+++ src/components/ImgPoring.tsx
@@ -1,15 +1,15 @@
-import PoringMinus1 from 'assets/images/poring_-1.png'
-import Poring0 from 'assets/images/poring_0.png'
-import Poring1 from 'assets/images/poring_1.png'
+import PoringGone from 'assets/images/poring_gone.png'
+import PoringGood from 'assets/images/poring_good.png'
+import PoringHit from 'assets/images/poring_hit.png'
 
 
 export default function ImgPoring({ state = 1 }: { state?: number }): JSX.Element {
     switch (state) {
         case 0:
-            return <img src={Poring0} className="inline-block" alt="fail" />;
+            return <img src={PoringHit} className="inline-block" alt="fail" />
         case -1:
-            return <img src={PoringMinus1} className="inline-block" alt="worst" />;
+            return <img src={PoringGone} className="inline-block" alt="worst" />
         default:
-            return <img src={Poring1} className="inline-block" alt="good" />;
+            return <img src={PoringGood} className="inline-block" alt="good" />
     }
 }
