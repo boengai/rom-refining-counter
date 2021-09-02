47595d8cf94dc57aae8afbf18b8aeb565d2dc637
Boe Khattiya <keerati.khat@gmail.com>
2021-09-02 22:31:05 +0700

    💄: remove hollgrehenn

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,24 +13,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    plugin(({ addUtilities }) => {
-      addUtilities({
-        '.bg-hollgregenn': {
-          content: '" "',
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          left: '0',
-          top: '0',
-          backgroundImage: 'url(assets/images/hollgrehenn.png)',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: '30vh',
-          backgroundAttachment: 'fixed',
-          opacity: '0.3'
-        }
-      })
-    })
-  ],
+  plugins: [],
 }
