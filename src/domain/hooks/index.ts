5058068ea157e0ce465d0f71ba99fb0a0f9e70b3
Boe Khattiya <keerati.khat@gmail.com>
2021-09-05 00:34:51 +0700

    ✨: nn prediction

---
+++ src/domain/hooks/index.ts
@@ -0,0 +1,1 @@
+export { default as useRefinePrediction } from './useRefinePrediction'
--- src/presentation/pages/home/context.tsx
+++ src/presentation/pages/home/context.tsx
@@ -1,5 +1,6 @@
-import { Refining, REFINING_TYPE } from "domain/entities"
-import { createContext, ReactNode, useContext, useMemo, useState } from "react"
+import { Refining, Refinings, REFINING_TYPE } from "domain/entities"
+import { useRefinePrediction } from "domain/hooks"
+import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from "react"
 
 const STORAGE_ITEM_REFINING_KEY = 'refining_history'
 
@@ -8,13 +9,14 @@ interface IHomeContextState {
 }
 
 const initialContextState: IHomeContextState = {
-    refining: []
+    refining: [],
 }
 
 interface IHomeContext extends IHomeContextState {
     addRefining: (v: REFINING_TYPE) => void
     clearRefining: () => void
     handleState: (s: Partial<IHomeContextState>) => void
+    predicted: REFINING_TYPE
     removeLastRefining: () => void
 }
 
@@ -31,36 +33,48 @@ function useHomeContext(): IHomeContext {
 
 export function HomeContextProvider({ children }: { children: ReactNode }): JSX.Element {
     const [contextState, setContextState] = useState<IHomeContextState>(initialContextState)
+    const { predicted, run, train } = useRefinePrediction()
+
     const handleContextState = (s: Partial<IHomeContextState>): void => {
         setContextState(prev => ({ ...prev, ...s }))
     }
-    const handleRefiningChange = (v: Array<Refining>) => {
-        handleContextState({ refining: v })
+    const handleRefiningChange = useCallback((v: Array<Refining>) => {
         localStorage.setItem(STORAGE_ITEM_REFINING_KEY, JSON.stringify(v))
-    }
+        handleContextState({ refining: v })
+    }, [])
 
     useMemo(() => {
         const historyJSON = localStorage.getItem(STORAGE_ITEM_REFINING_KEY)
         if (historyJSON) {
-            const history: Array<Refining> = JSON.parse(historyJSON)
+            const history = Refinings.fromJSON(JSON.parse(historyJSON))
             handleContextState({ refining: history })
+            train(history)
         }
 
-    }, [])
+    }, [train])
 
     return <HomeContext.Provider value={{
         ...contextState,
-        addRefining: (v: REFINING_TYPE) => {
-            handleRefiningChange([...contextState.refining, Refining.fromType(v)])
-        },
-        clearRefining: () => {
+        predicted,
+        addRefining: useCallback((v: REFINING_TYPE) => {
+            const refined = Refining.fromType(v)
+            handleRefiningChange([...contextState.refining, refined])
+            if (refined.isEqual(predicted)) {
+                run(contextState.refining)
+                return
+            }
+
+            train(contextState.refining)
+        }, [contextState.refining, handleRefiningChange, predicted, run, train]),
+        clearRefining: useCallback(() => {
             handleRefiningChange([])
-        },
-        removeLastRefining: () => {
+        }, [handleRefiningChange]),
+        removeLastRefining: useCallback(() => {
             const cur = [...contextState.refining]
             cur.pop()
             handleRefiningChange(cur)
-        },
+            run(cur)
+        }, [contextState.refining, handleRefiningChange, run]),
         handleState: handleContextState
     }}>
         {children}
